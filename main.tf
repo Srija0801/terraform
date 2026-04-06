c785d7c1fd52f170fa9ec7ea324db51960bf6b28
Srija0801 <Srija082006@gmail.com>
2026-04-06 23:30:29 +0530

    Fix the error

--- main.tf
+++ main.tf
@@ -1,71 +1,23 @@
-# Variable to control Kubernetes resources
-variable "enable_k8s" {
-  default = true
+provider "kubernetes" {
+config_path = "~/.kube/config"
 }
 
-# -------------------------------
-# MongoDB Deployment
-# -------------------------------
-resource "kubernetes_deployment_v1" "mongodb" {
-  count = var.enable_k8s ? 1 : 0
+resource "kubernetes_pod" "mongodb_pod" {
+metadata {
+name = "mongodb-pod"
+labels = {
+app = "mongodb"
+}
+}
+spec {
+container {
+image = "mongo"
+name = "mongodb"
 
-  metadata {
-    name = "mongodb-deployment"
-    labels = {
-      app = "mongodb"
-    }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "mongodb"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "mongodb"
-        }
-      }
-
-      spec {
-        container {
-          name  = "mongodb"
-          image = "mongo:latest"
-
-          port {
-            container_port = 27017
-          }
-        }
-      }
-    }
+  port {
+    container_port = 27017
   }
 }
 
-# -------------------------------
-# MongoDB Service
-# -------------------------------
-resource "kubernetes_service_v1" "mongodb_service" {
-  count = var.enable_k8s ? 1 : 0
-
-  metadata {
-    name = "mongodb-service"
-  }
-
-  spec {
-    selector = {
-      app = "mongodb"
-    }
-
-    port {
-      port        = 27017
-      target_port = 27017
-    }
-
-    type = "NodePort"
-  }
+}
 }
--- provider.tf
+++ provider.tf
@@ -1,15 +1,1 @@
-variable "enable_k8s" {
-  default = true
-}
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-
-  # 👇 THIS IS THE FIX
-  alias = "local"
-}
-
-# Dummy provider for CI (prevents loading error)
-provider "kubernetes" {
-  alias = "ci"
-}
